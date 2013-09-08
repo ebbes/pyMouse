378c3fd8b6c24854aa58a1984580c328d30cf804
Eric Hutter <hutter.eric@gmail.com>
2013-09-08 22:59:28 +0200

    Removed stupid method

--- Led.py
+++ Led.py
@@ -1,19 +1,9 @@
 import os
-import time
 
 class Led:
     def __init__(self, descriptor):
         self.led = descriptor
         self.state = b"none"
-    
-    def blink(self):
-        if self.led is None:
-            return
-        
-        state = self.state == b"default-on"
-        self.set_value(not state)
-        time.sleep(1/50)
-        self.set_value(state)
     
     def set_value(self, value):
         if self.led is None:
